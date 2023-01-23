763358e9f126f2c4576961439fdad9b8cb5609a7
LarsBoertjes <120394687+LarsBoertjes@users.noreply.github.com>
2023-01-23 11:11:21 +0100

    Add files via upload

---
+++ Beverbende.py
@@ -0,0 +1,31 @@
+# Beverbende CS101 Portfolio Project
+
+# let's you play single player Beverbende against 2 or more Computer Players
+# You will see 4 uncovered values on your screen which represent your cards with a value
+# (first make it while you see the cards, later make it hidden)
+# You will see the 2 outside cards for a very brief moment
+# There are two stacks of cards, one covered, one uncovered.
+
+# Option A: player takes from covered stack
+# Option: it is a normal card, player choses to trade with one of his own cards, or put the card on the uncovered stack
+# Option: it is a special card, player choses to use the special card with its action, or put the card on the uncovered stack
+
+# Option B: player takes from uncovered stack
+# Player has to trade card with one of his 4 own card, choses himself which one
+
+# Special Cards:
+# Trade: player who draws this card can trade against one of other players cards
+# Look: player can look up the value of one of his 4 cards
+# Draw Twice: Gets to draw, if player doesn't want the card, he can draw again.
+
+# Game goes on until 1 player calls 'last round', then; every other player gets to play one more round
+# After this round, cards have to be shown. 
+# Score is calculated by value of normal cards.
+# If there are still special cards belonging to a player, the player must trade them with the top card on the uncovered stack
+
+# Steps:
+# Build deck
+# Assign 4 random cards from deck to every player
+# Uncover 4 random cards
+
+
